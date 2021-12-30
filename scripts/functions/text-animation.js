577c8c1f68bf618ddf34f5d8cdfc68f6d46aef48
Yuto Yamakita <yuvto.0420@gmail.com>
2021-12-29 20:15:04 -0800

    add text animation

---
+++ scripts/functions/text-animation.js
@@ -0,0 +1,61 @@
+let words = document.querySelectorAll(".word");
+let wordArray = [];
+let currentWord = 0;
+
+words[currentWord].style.opacity = 0;
+for(let i = 0; i < words.length; i++){
+    splitLetters(words[i]);
+}
+
+
+function changeWord(){
+    let cw = wordArray[currentWord]; //cw: current word
+    let nw = (currentWord == words.length-1)? wordArray[0] : wordArray[currentWord + 1];
+    //nw: next word
+
+    for(let i = 0; i < cw.length ; i++){
+        animateLetterOut(cw, i);
+    }
+
+    for(let i = 0; i < nw.length; i++){
+        nw[i].className = "letter behind";
+        nw[0].parentElement.style.opacity = 1;
+        // console.log(nw[0].parentElement)
+        animateLetterIn(nw, i);
+    }
+
+    currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i){
+    setTimeout(() => {
+        cw[i].className = "letter out";
+    }, i*90);
+}
+
+function animateLetterIn(nw, i){
+    setTimeout(() => {
+        nw[i].className = "letter in";
+    }, (i*90)+350);
+}
+
+function splitLetters(word){
+    let content = word.innerHTML;
+    word.innerHTML = '';
+    let letters = [];
+
+    for(let i = 0; i < content.length; i++){
+        let letter = document.createElement("span");
+        letter.className = "letter";
+        letter.innerHTML = content.charAt(i);
+        word.appendChild(letter);
+        letters.push(letter);
+    }
+
+    wordArray.push(letters);
+    // console.log(wordArray);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
+
