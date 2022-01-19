a90c4b316701b128da5e6c8912ee6aed92a68ccc
Yuto Yamakita <yuvto.0420@gmail.com>
2022-01-18 22:37:57 -0800

    add event to project section

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+const projectCards = document.querySelectorAll(".project__card");
+const behindCards = document.querySelectorAll(".project__behind-card");
+
+
+function showUp(){
+    
+    behindCards.forEach(behindCard => {
+        behindCard.classList.add("show");
+    })
+
+    this.style.display = "none";
+}
+
+function hide(){
+    this.classList.remove("show");
+    
+}
+
+
+projectCards.forEach(projectCard =>{
+    projectCard.addEventListener("click", showUp);
+});
+
+behindCards.forEach(behindCard =>{
+    behindCard.addEventListener("click", hide);
+})
+
+
 
