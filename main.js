9d757abfdec545d5a9a8efb58717352c34b3acfd
Yuto Yamakita <yuvto.0420@gmail.com>
2022-01-19 20:02:29 -0800

    modify a click events

--- main.js
+++ main.js
@@ -1,29 +1,52 @@
-const projectCards = document.querySelectorAll(".project__card");
-const behindCards = document.querySelectorAll(".project__behind-card");
+let projectCards = document.querySelectorAll(".project__card");
 
-
-function showUp(){
-    
-    behindCards.forEach(behindCard => {
-        behindCard.classList.add("show");
-    })
-
-    this.style.display = "none";
-}
-
-function hide(){
-    this.classList.remove("show");
-    
-}
-
-
+let index;
 projectCards.forEach(projectCard =>{
-    projectCard.addEventListener("click", showUp);
-});
+    projectCard.addEventListener("click", ()=>{
+        index = [].slice.call(projectCards).indexOf(projectCard);
+        let behindCards = document.querySelectorAll(".project__behind-card")[index];
 
-behindCards.forEach(behindCard =>{
-    behindCard.addEventListener("click", hide);
+        let style = behindCards.currentStyle || document.defaultView.getComputedStyle(behindCards, '');
+        if(style.display == "none"){
+            behindCards.classList.add("show");
+        }else{
+            behindCards.classList.remove("show");
+        }
+        
+    })
 })
+
+// projectCards.forEach(function(target){
+//     target.addEventListener("click",  function(e){
+//         console.log(e.currentTarget);
+//         let targetCard = e.currentTarget;
+        
+//         targetCard.firstElementChild.classList.add("show");
+//     })
+// })
+
+
+// function showUp(){
+    
+//     behindCards.forEach(behindCard => {
+//         console.log(this);
+//         behindCard.classList.toggle("show");
+//         this.stopPropagation();
+//     })
+// }
+
+// function hide(){
+//     this.classList.toggle("show");
+// }
+
+
+// projectCards.forEach(projectCard =>{
+//     projectCard.addEventListener("mouseover", showUp);
+// });
+
+// behindCards.forEach(behindCard =>{
+//     behindCard.addEventListener("click", hide);
+// })
 
 
 
