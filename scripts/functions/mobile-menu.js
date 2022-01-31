7daa4085ca2ad39c27535f79a1f2417a94e1f953
Yuto Yamakita <yuvto.0420@gmail.com>
2022-01-30 22:11:13 -0800

    Edit mobile-menu, add function to close menu and navigate

--- scripts/functions/mobile-menu.js
+++ scripts/functions/mobile-menu.js
@@ -2,8 +2,12 @@ class MobileMenu{
     constructor(){
         this.DOM = {};
         this.DOM.btn = document.querySelector(".mobile-menu__btn");
+        // this.DOM.items = document.querySelectorAll(".mobile-menu__item");
+        // console.log(this.DOM.item);
         this.DOM.container = document.querySelector("#global-container");
         this.eventType = this._getEventType();
+        // this._closeMenu();
+        // this._closeMenu2();
         this._addEvent();
     }
 
@@ -14,6 +18,19 @@ class MobileMenu{
     _toggle(){
         this.DOM.container.classList.toggle("menu-open");
     }
+
+    // _closeMenu2(){
+    //     this.DOM.container.classList.remove("menu-open")
+    // }
+
+    // _closeMenu(){
+    //     this.DOM.items.forEach(item => {
+    //          item.addEventListener(
+    //            "click",
+    //            _closeMenu2()
+    //          );
+    //     });
+    // }
     
     _addEvent(){
         this.DOM.btn.addEventListener("click", this._toggle.bind(this));
@@ -21,4 +38,14 @@ class MobileMenu{
 }
 
 new MobileMenu();
+
+const menu = document.querySelector("#global-container");
+const items = document.querySelectorAll(".mobile-menu__item");
+
+function closeMenu(){
+    menu.classList.remove("menu-open");
+}
+items.forEach(item =>{
+    item.addEventListener("click", closeMenu)
+})
 
